c0f08c7a69479a593ee91c5b9a25d785baa68bc6
Shreyas S Shetty <shreyassshetty222@gmail.com>
2025-11-13 18:50:35 +0530

    CI: driver_factory uses BROWSER/HEADLESS and writes chromedriver.log

--- selenium_web/utils/driver_factory.py
+++ selenium_web/utils/driver_factory.py
@@ -1,19 +1,47 @@
-﻿import os
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
-def create_driver(headless: bool = True):
-    opts = ChromeOptions()
-    if headless:
-        opts.add_argument("--headless=new")
-    opts.add_argument("--no-sandbox")
-    opts.add_argument("--disable-dev-shm-usage")
-    opts.add_argument("--disable-gpu")
-    opts.add_argument("--window-size=1366,768")
+def _is_true(val):
+    return str(val).lower() in ('1','true','yes')
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=opts)
-    driver.implicitly_wait(5)
+def create_driver(browser=None, headless=None):
+    browser = (browser or os.environ.get('BROWSER') or 'chrome').lower()
+    headless = True if headless is True else (_is_true(headless or os.environ.get('HEADLESS', 'false')))
+
+    if browser == 'firefox':
+        options = FirefoxOptions()
+        if headless:
+            options.headless = True
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        options = ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1080')
+        options.add_argument('--disable-extensions')
+        options.add_argument('--disable-infobars')
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--disable-software-rasterizer')
+        if headless:
+            try:
+                options.add_argument('--headless=new')
+            except Exception:
+                options.add_argument('--headless')
+        # ask webdriver-manager to install a driver; capture chromedriver verbose log
+        driver_path = ChromeDriverManager().install()
+        service = ChromeService(executable_path=driver_path, log_path='chromedriver.log')
+        driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        driver.maximize_window()
+    except Exception:
+        pass
     return driver
